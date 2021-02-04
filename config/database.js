26a8e2e0fe8e4fdae83feabad0feaad187f0d059
Von <vstrickl.git@gmail.com>
2021-02-04 00:33:01 -0800

    Added Cloudinary Plugin

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'cms'),
+        database: env('DATABASE_NAME', 'zoomclassbillingapp'),
         username: env('DATABASE_USERNAME', 'vonst'),
         password: env('DATABASE_PASSWORD', 'vs5678!'),
         ssl: env.bool('DATABASE_SSL', false),
